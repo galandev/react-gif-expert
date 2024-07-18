c2f38bb2e4b5263473aa0654c4f85c87d3900362
Roberto <rgalanllorca@me.com>
2024-07-18 19:46:33 +0200

    First Commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GiftExpertApp = () => {
+
+  const [ categories, setCategories ] = useState(['Goku']);
+  
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+
+      <AddCategory 
+        onNewCategory={ onAddCategory }
+      />
+
+        { 
+          categories.map( (category) => (
+            <GifGrid 
+              key={ category } 
+              category={ category} />
+          )) 
+        }
+    </>
+  )
+}
